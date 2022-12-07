2a4a63527644996b2832f5a8b976b3330aa886d0
= <gabrieldouglas.barbosa@hotmail.com>
2022-12-07 10:11:36 -0300

    coment

--- SERVER/database/seeders/DatabaseSeeder.php
+++ SERVER/database/seeders/DatabaseSeeder.php
@@ -16,10 +16,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
-            'name' => 'admin',
-            'email' => 'admin@unitri.com',
-            'password' => bcrypt('admin'),
-        ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'admin',
+        //     'email' => 'admin@unitri.com',
+        //     'password' => bcrypt('admin'),
+        // ]);
     }
 }
